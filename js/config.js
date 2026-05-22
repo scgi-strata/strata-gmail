9f314a55ec1cfff460b1caca7533c795f577a2bd
scgi-strata <josephdemaala.stratagroup@gmail.com>
2026-05-22 22:29:52 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
-  CLIENT_ID: 'YOUR_GOOGLE_CLIENT_ID_HERE',
-  SPREADSHEET_ID: 'YOUR_SPREADSHEET_ID_HERE',
+  CLIENT_ID: '824574272042-2hp87h45sepvm9cseruqn0l7ok4cmbts.apps.googleusercontent.com',
+  SPREADSHEET_ID: '17mOS1frluGNdAqS2jFcKha9TJ72Aiwk9Tsp2-M4grC4',
   SCOPES: [
     'https://www.googleapis.com/auth/gmail.readonly',
     'https://www.googleapis.com/auth/spreadsheets',
